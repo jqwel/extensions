f900c547fd6f8c150028fbe967b4854fc5135d29
jqwel <jqwel@YLZ.local>
2025-03-14 18:29:38 +0800

    add README

---
+++ README.md
@@ -0,0 +1,27 @@
+# Extensions
+
+[![License](https://img.shields.io/github/license/jqwel/extensions)](LICENSE)
+[![Issues](https://img.shields.io/github/issues/jqwel/extensions)](https://github.com/jqwel/extensions/issues)
+[![Version](https://img.shields.io/github/v/release/jqwel/extensions)](https://github.com/jqwel/extensions/releases)
+[![GitHub stars](https://img.shields.io/github/stars/jqwel/extensions)](https://github.com/jqwel/extensions)
+
+简洁实用的扩展工具集合
+
+## 📦 项目简介
+
+本仓库包含一系列实用的扩展工具:
+- 加载不同的JS和CSS, 更清晰地标注页面广告, 帮助缺乏网络经验的人群避开广告陷阱。
+
+## ✨ 主要特性
+
+- 支持[Tampermonkey](https://www.tampermonkey.net/)脚本, 安装URL:https://raw.githubusercontent.com/jqwel/extensions/refs/heads/main/dist/focus_booster.user.js, git文件[focus_booster.user.js](dist/focus_booster.user.js)。模版目录[tampermonkey](run_sh/tampermonkey)。
+- 支持Chrome扩展,在[focus_booster](focus_booster)文件夹开发者模式加载。
+
+## 🚀 快速开始
+
+### 安装方法
+
+#### 浏览器扩展
+1. 克隆本仓库
+   ```bash
+   git clone https://github.com/jqwel/extensions.git
---
+++ run_sh/css_data/main.go
@@ -0,0 +1,224 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+)
+
+//go:embed part1.css
+var part1css string
+
+//go:embed part2.css
+var part2css string
+
+//go:embed part3.css
+var part3css string
+
+//go:embed root.css
+var rootcss string
+
+func main() {
+	var input []OutputEntry = []OutputEntry{
+		{
+			Filename: "all.css",
+			Selectors: []string{
+				`.__focus_booster`,
+				`div[id^="google_ads_iframe_"]`,
+				// ".adsbygoogle",
+				`div:has(> iframe[src*="googleads.g.doubleclick.net/"])`,
+				`div:has(> iframe[src^="https://pos.baidu.com/"])`,
+				`div:has(> iframe[src^="//pos.baidu.com/"])`,
+				// `div[class$='-ad']`,
+				// `div[class*='-ad-']`,
+				`.advertise`,
+				`div:has(> img[class*="-ad-"]) a`,
+				`a:has(> img[class*="-ad-"])`,
+				`div[id*="_ad_"]`,
+			},
+			Prefix: rootcss,
+		},
+		{
+			Filename: "2345.css",
+			Selectors: []string{
+				".xxl-ad-360",
+			},
+		},
+		{
+			Filename: "360.css",
+			Selectors: []string{
+				".uni-flow-card:has(.ad)",
+				".cube-mod:has(.ad-flag)",
+			},
+		},
+		{
+			Filename: "baidu.css",
+			Selectors: []string{
+				".c-container:has(.ec-tuiguang):not(:has(div[data-cmatchid]))", // 排除5
+				".cr-content:has(.ec-tuiguang):not(:has(div[data-cmatchid]))",  // 排除5
+				`.mediago-ad-wrapper`,
+				`div[class^="unionAd_"]`,
+				`div[data-cmatchid]:has(.ec-tuiguang)`,
+			},
+		},
+		{
+			Filename: "bilibili.css",
+			Selectors: []string{
+				".bili-video-card__wrap:has(.bili-video-card__info--ad)",
+				`.bili-video-card__wrap:has(.bili-video-card__image--link[href*="cm.bilibili.com/cm/api"])`,
+				`.ad-report`,
+			},
+		},
+		{
+			Filename: "bing.css",
+			Selectors: []string{
+				`.sb_adTA`,
+			},
+		},
+		{
+			Filename: "duckduckgo.css",
+			Selectors: []string{
+				`li[data-layout="ad"]`,
+			},
+		},
+		{
+			Filename: "facebook.css", // 切换到中文
+			Selectors: []string{
+				`.x1yztbdb:has(a[href^="/ads/about/?"])`,
+				// `.x1yztbdb:has(a[href*="#?"]:not([href*="%2C"]))`,
+			},
+		},
+		{
+			Filename: "google.css",
+			Selectors: []string{
+				`div[data-text-ad="1"]`,
+			},
+		},
+		{
+			Filename: "msn.css",
+			Selectors: []string{
+				`/deep/cs-native-ad-card:has(.ad-label-text)`,
+			},
+		},
+		{
+			Filename: "mydrivers.css",
+			Selectors: []string{
+				`li:has(.c)`,
+				`.good_ths li`,
+			},
+			AdditionalData: `body {
+    background-color: black !important;
+}`,
+		},
+		{
+			Filename: "netease.css",
+			Selectors: []string{
+				`.ad_text`,
+				`.mod_js_ad`,
+				`.common_ad_item`,
+				`.right_ad_item`,
+				`.post_recommend_ad`,
+			},
+		},
+		{
+			Filename: "qq.css",
+			Selectors: []string{
+				`.ad-cell-common`,
+				`.channel-feed-item:has(.adCode)`,
+				`.rectangle-ad-channel`,
+				`div[adconfig_key]`,
+			},
+		},
+		{
+			Filename: "sina.css",
+			Selectors: []string{
+				`.sinaads:not(.sinaads-fail)`,
+				`.sinaads-float`,
+			},
+		},
+		{
+			Filename: "so.css",
+			Selectors: []string{
+				`.e-buss li`,
+			},
+		},
+		{
+			Filename: "sogou.css",
+			Selectors: []string{
+				`.sponsored-ams > div`,
+				`.sdk-ad-side-banner-hover`,
+				`.art-container:has(.feed-item-close)`,
+				`[class^="shopWindow_shopWindowAd"]:not([class^="shopWindow_shopWindowAd"] [class^="shopWindow_shopWindowAd"])`, // `:is([class^="shopWindow_shopWindowAd"])`,
+				`.sdk-ad`,
+			},
+		},
+		{
+			Filename: "v2ex.css",
+			Selectors: []string{
+				`.wwads-cn`,
+			},
+		},
+		{
+			Filename: "weibo.css",
+			Selectors: []string{
+				`.vue-recycle-scroller__item-view:has(.wbpro-tag-img)`,
+				`:is([class*=" TipsAd_picture_"], [class^="TipsAd_picture_"])`,
+			},
+		},
+		{
+			Filename: "youtube.css",
+			Selectors: []string{
+				`ytd-ad-slot-renderer`,
+				`#player-ads`,
+				`#masthead-ad:not(:has(ytd-ad-slot-renderer))`,
+			},
+		},
+		{
+			Filename: "x.css",
+			Selectors: []string{
+				`div[data-testid="placementTracking"]:has([data-testid*="impression"])`,
+			},
+		},
+	}
+	// Part 1
+	for _, entry := range input {
+		var pre = entry.Prefix
+		var part1 = GenPart(entry.Selectors, "", part1css)
+		var part2 = GenPart(entry.Selectors, ":hover", part2css)
+		var part3 = GenPart(entry.Selectors, "::after", part3css)
+		if pre != "" {
+			pre += "\n\n"
+		}
+		var result = fmt.Sprintf("%s%s\n\n%s\n\n%s\n", pre, part1, part2, part3)
+		// Part 2
+		// Part 3
+		if entry.AdditionalData != "" {
+			result += fmt.Sprintf("\n%s\n", entry.AdditionalData)
+		}
+		// fmt.Println(entry.Filename)
+		// fmt.Println(result)
+		err := os.WriteFile(fmt.Sprintf("../../focus_booster/css/%s", entry.Filename), []byte(result), 0644)
+		if err != nil {
+			fmt.Println("写入文件失败:", err)
+			return
+		}
+	}
+}
+
+func GenPart(selectors []string, decorator string, content string) string {
+	var result string
+	for i, selector := range selectors {
+		if i != 0 {
+			result += ",\n"
+		}
+		result += selector + decorator
+	}
+	return result + " " + content
+}
+
+type OutputEntry struct {
+	Filename       string
+	Prefix         string
+	Selectors      []string
+	AdditionalData string
+}
